5a056f91c3c79f92f1f9e7efb0c7b4ef92f38bc0
sandip ahire <sandipahire34@gmail.com>
2021-09-20 18:10:41 +0530

    Academy Project Console Application using C# .Net And MSSQL Server

---
+++ AcademyFunctions/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace AcademyFunctions
+{
+    public class Class1
+    {
+        static SqlConnection con;
+        static SqlCommand cmd;
+        static SqlDataReader dr;
+
+
+        public static void CreateConnection()
+        {
+            try
+            {
+                string constr = "Data Source=DESKTOP-2UMN69M;Initial Catalog=Academydb; User ID=sa;Password=pass@123";
+                con = new SqlConnection();
+
+                con.ConnectionString = constr;
+                // con.Open();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Please check connection details");
+            }
+
+
+        }
+
+        public static void Display()
+        {
+            con.Open();
+            string query = "Select * from student";
+            cmd = new SqlCommand(query, con);
+            dr = cmd.ExecuteReader();
+            Console.Write("SRollno\tSName\tSContact\tSAddress\tSEmail\tSTrainer\tSbatchno\tSmarks1\tSmarks2\n");
+            while (dr.Read())
+            {
+                Console.Write("{0}\t\t{1}\t\t{2}\t\t{3}\t\t{4}\t\t{5}\t\t{6}\t\t{7}\n", dr[0], dr[1], dr[2], dr[3], dr[4], dr[5], dr[6], dr[7]);
+            }
+            con.Close();
+
+        }
+
+        //    DispalyTrainer(){
+    }
+
+
+    // DisplayBatch(){
+}
+        public static void insertRecords(int rollno, string name, string contact, string address, string email, string trainer, string batch,int marks1,int marks2)
+        {
+            con.Open();
+            string query = "insert into student values(" + rollno + ",'" + name + "','" + contact + "','" + address + "','" + email + "','" + trainer + "','" + batch + "'," + marks1 + "," + marks2 + ")";
+            cmd = new SqlCommand(query, con);
+            cmd.ExecuteNonQuery();
+            //try
+            //{
+            //    con.Open();
+            //    cmd.ExecuteNonQuery();
+            //}
+            //catch (System.Data.SqlClient.SqlException ex)
+            //{
+            //    string msg = "Insert Error:";
+            //    msg += ex.Message;
+            //}
+            //finally
+            //{
+            //    con.Close();
+            //}
+
+        }
+    }
+}
---
+++ AcademyProj/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using AcademyFunctions;
+namespace AcademyProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char reply = 'y';
+            Class1.CreateConnection();
+            Console.WriteLine("Connection with database Successful..");
+            Console.ReadLine();
+            do
+            { 
+            Console.WriteLine("=============================MAIN MENU===============================");
+            Console.WriteLine("1.ADDING RECORDS");
+            Console.WriteLine("2.UPDATING RECORDS");
+            Console.WriteLine("3.DELETING RECORDS");
+            Console.WriteLine("4.DISPLAYING RECORDS");
+            Console.WriteLine("5.EXITING APPLICATION");
+
+            Console.WriteLine("ENTER YOUR CHOICE:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+                int rollno;
+                string name;
+                string contact;
+                string address;
+                string email;
+                string trainer;
+                string batch;
+                int marks1, marks2;
+            switch (choice)
+            {
+                case 1:
+                        Console.WriteLine("Enter Your details Rollno, Name, contact, address, email, trainerid, batch, marks1, and marks2");
+                        rollno = Convert.ToInt32(Console.ReadLine());
+                        name = Console.ReadLine();
+                        // display trainer and batch details 
+
+                        //Class1.DisplayTrainer();
+                        //Class1.DisplayBatch();
+
+                        Class1.insertRecords(rollno, name,contact, address, email, trainerid, batch, marks1, marks2);
+                        break;
+                case 2: break;
+                case 3: break;
+                case 4:
+                    Console.WriteLine("Displaying the Records");
+                    Class1.Display();
+                    break;
+                case 5:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Choice");
+                    break;
+            }
+
+            Console.WriteLine("Do you wish to continue");
+            reply = Convert.ToChar(Console.ReadLine());
+        }while(reply=='y');
+            Console.ReadLine();
+        }
+    }
+}
