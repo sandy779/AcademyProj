835da427c12c7e9e55b086f9f1f9155ef8953eaf
sandip ahire <sandipahire34@gmail.com>
2021-09-24 11:54:02 +0530

    Project Updated

--- AcademyFunctions/Class1.cs
+++ AcademyFunctions/Class1.cs
@@ -10,6 +10,7 @@ namespace AcademyFunctions
     {
         static SqlConnection con;
         static SqlCommand cmd;
+        static SqlCommand cmd1;
         static SqlDataReader dr;
 
 
@@ -28,7 +29,7 @@ namespace AcademyFunctions
                 Console.WriteLine("Please check connection details");
             }
 
-
+         
         }
 
         public static void Display()
@@ -37,44 +38,162 @@ namespace AcademyFunctions
             string query = "Select * from student";
             cmd = new SqlCommand(query, con);
             dr = cmd.ExecuteReader();
-            Console.Write("SRollno\tSName\tSContact\tSAddress\tSEmail\tSTrainer\tSbatchno\tSmarks1\tSmarks2\n");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------");
+            Console.Write("Rollno\tName\tContact\t\tAddress\t\t\tTrainerid\t\t\tBatchId\n");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------");
             while (dr.Read())
             {
-                Console.Write("{0}\t\t{1}\t\t{2}\t\t{3}\t\t{4}\t\t{5}\t\t{6}\t\t{7}\n", dr[0], dr[1], dr[2], dr[3], dr[4], dr[5], dr[6], dr[7]);
+                Console.Write("{0}\t{1}\t{2}\t\t{3}\t\t\t{4}\t\t{5}\n", dr[0], dr[1], dr[2], dr[3], dr[4],dr[5]);
+
             }
+            Console.WriteLine("==============================================================================================================");
             con.Close();
 
         }
 
-        //    DispalyTrainer(){
-    
+        public static void DisplayTrainer(string trainerid)
+        {
+            con.Open();
+            string query = "Select TrainerName from Trainer where TrainerId="+trainerid+"";
+            cmd = new SqlCommand(query, con);
+            dr=   cmd.ExecuteReader();
+            Console.WriteLine("TrainerName");
+            while (dr.Read())
+            {
+                Console.WriteLine("{0}", dr[0]);
+            }
+            con.Close();
+        }
+        public static void DisplayBatch(string batchid)
+        {
+            con.Open();
+            string query = "select BatchName from Batch b join student stud on b.BatchId = stud.BatchId where stud.BatchId = '" + batchid + "'";
+
+            cmd = new SqlCommand(query, con);
+            dr = cmd.ExecuteReader();
+            Console.WriteLine("TrainerName");
+            while (dr.Read())
+            {
+                Console.WriteLine("{0}", dr[0]);
+            }
+            con.Close();
+        }
 
 
-    // DisplayBatch(){
+        // DisplayBatch()
 
-public static void insertRecords(int rollno, string name, string contact, string address, string email, string trainer, string batch, int marks1, int marks2)
+public static void insertRecords(int rollno, string name, string contact, string address,  string trainer, string batch)
 {
     con.Open();
-    string query = "insert into student values(" + rollno + ",'" + name + "','" + contact + "','" + address + "','" + email + "','" + trainer + "','" + batch + "'," + marks1 + "," + marks2 + ")";
+    string query = "insert into student values(" + rollno + ",'" + name + "','" + contact + "','" + address + "','" + trainer + "','" + batch + "')";
+    string query2="insert into Exam values("+rollno+",null)";
     cmd = new SqlCommand(query, con);
+    cmd1 = new SqlCommand(query2, con);
     cmd.ExecuteNonQuery();
+            cmd1.ExecuteNonQuery();
 
-   
-            try
-            {
-               
-               cmd.ExecuteNonQuery();
-            }
-            catch (System.Data.SqlClient.SqlException ex)
-            {
-                string msg = "Insert Error:";
-                msg += ex.Message;
-            }
-            finally
-            {
-                con.Close();
-            }
 
+            /* try
+             {
+
+                cmd.ExecuteNonQuery();
+             }
+             catch (System.Data.SqlClient.SqlException ex)
+             {
+                 string msg = "Insert Error:";
+                 msg += ex.Message;
+             }
+             finally
+             {
+                 con.Close();
+             }*/
+            con.Close();
+
+        }
+        public static void deleterecord(int rollno)
+        {
+            con.Open();
+            string query = "delete from student where SRollno="+rollno+"";
+            cmd=new SqlCommand(query, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public static void updateContact(string contact,int rollno)
+        {
+            con.Open();
+            string query = "update student set SContact=" + contact + "where SRollno="+rollno+"";
+            cmd = new SqlCommand(query, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public static void updatemarks(int marks,int rollno)
+        {
+            con.Open();
+            string query = "update Exam set Marks=@marks where SRollno=@r";
+            cmd = new SqlCommand(query, con);
+            cmd.Parameters.Add(new SqlParameter("marks", marks));
+            cmd.Parameters.Add(new SqlParameter("r", rollno));
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public static void topfive(string batchid1)
+        {
+            con.Open();
+            //string query = "select * from vwtop5";
+            cmd = new SqlCommand("prctoo5", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("batchid", batchid1));
+           dr= cmd.ExecuteReader();
+            Console.WriteLine("====================================TOP 5 MERIT LIST IN SPECIFIC BATCH=====================================================");
+            Console.Write("STUDENT NAME\t\tMARKS OBTAINED\n");
+            while (dr.Read())
+            {
+                Console.WriteLine("{0}\t\t\t\t{1}\t\t{2}\n", dr[0],dr[1],dr[2]);
+            }
+            con.Close();
+        }
+        public static void topten()
+        {
+            con.Open();
+            string query = "select top 10 SName,Marks,BatchId from student stud join Exam ex on stud.Srollno=ex.SRollno order by Marks desc";
+            cmd = new SqlCommand(query, con);
+            dr = cmd.ExecuteReader();
+            Console.WriteLine("====================================TOP 10 MERIT LIST IN ALL BATCHES=====================================================");
+            Console.Write("STUDENT NAME\t\tMARKS OBTAINED\t\t\t\tBATCHID\n");
+            while (dr.Read())
+            {
+                Console.WriteLine("{0}\t\t\t\t{1}\t\t\t\t{2}\n", dr[0], dr[1],dr[2]);
+            }
+            con.Close();
+        }
+
+        public static void Top()
+        {
+            con.Open();
+            string query = "select* from view1";
+            cmd = new SqlCommand(query, con);
+            dr = cmd.ExecuteReader();
+            Console.WriteLine("====================================TOP TRAINER AND TOP BATCH =====================================================");
+            Console.Write("TOP TRAINER ID\t\tTOP BATCH ID\n");
+            while (dr.Read())
+            {
+                Console.WriteLine("{0}\t\t\t\t{1}\n", dr[0], dr[1]);
+            }
+            con.Close();
+        }
+        public static void lowscore()
+        {
+            con.Open();
+            string query = "select top 1 BatchId from  student stud join Exam ex on stud.SRollno=ex.SRollno group by stud.BatchId having avg(Marks)<70";
+            cmd = new SqlCommand(query, con);
+            dr = cmd.ExecuteReader();
+            Console.WriteLine("====================================LOW SCORE BATCH=====================================================");
+            Console.Write("LOW PERFORMANCE BATCH \n");
+            while (dr.Read())
+            {
+                Console.WriteLine("{0}\n", dr[0]);
+            }
+            con.Close();
         }
     }
 }
--- AcademyProj/Program.cs
+++ AcademyProj/Program.cs
@@ -7,75 +7,172 @@ using System.Data.SqlClient;
 using AcademyFunctions;
 namespace AcademyProj
 {
+   
     class Program
     {
+       
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            int ch;
             char reply = 'y';
             Class1.CreateConnection();
-            Console.WriteLine("Connection with database Successful..");
+            
+            
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("******************************************WELCOME TO TQ ACADEMY***********************************************");
+            Console.WriteLine();
             Console.ReadLine();
-            do
-            { 
-            Console.WriteLine("=============================MAIN MENU===============================");
-            Console.WriteLine("1.ADDING RECORDS");
-            Console.WriteLine("2.UPDATING RECORDS");
-            Console.WriteLine("3.DELETING RECORDS");
-            Console.WriteLine("4.DISPLAYING RECORDS");
-            Console.WriteLine("5.EXITING APPLICATION");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+           Console.WriteLine("===================================SELECT OPERATION==================================================");
+         Console.WriteLine("\t\t1.Student Operations");
+            Console.WriteLine("\t\t2.Performance Reports");
+            ch = Convert.ToInt32(Console.ReadLine());
+            
 
-            Console.WriteLine("ENTER YOUR CHOICE:");
-            int choice = Convert.ToInt32(Console.ReadLine());
-                int rollno;
-                string name;
-                string contact;
-                string address;
-                string email;
-                string trainerid;
-                string batch;
-                int marks1, marks2;
-            switch (choice)
+            
+;            switch (ch)
             {
                 case 1:
-                        Console.WriteLine("Enter Your details Rollno, Name, contact, address, email, trainerid, batch, marks1, and marks2");
-                        rollno = Convert.ToInt32(Console.ReadLine());
-                        name = Console.ReadLine();
-                        contact = Console.ReadLine();
-                        address = Console.ReadLine();
-                        email = Console.ReadLine();
-                        trainerid = Console.ReadLine();
-                        batch = Console.ReadLine();
-                        marks1 = Convert.ToInt32( Console.ReadLine());
-                        marks2 = Convert.ToInt32(Console.ReadLine());
-                        // display trainer and batch details 
 
-                        //Class1.DisplayTrainer();
-                        //Class1.DisplayBatch();
+                    do
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        Console.WriteLine("=============================MAIN MENU===============================");
+                        Console.WriteLine();
+                        Console.WriteLine("1.ADDING RECORDS");
+                        Console.WriteLine("2.UPDATE STUDENT CONTACT");
+                        Console.WriteLine("3.DELETING RECORDS");
+                        Console.WriteLine("4.DISPLAYING RECORDS");
+                        Console.WriteLine("5.VIEW TRAINER NAME");
+                        Console.WriteLine("6.VIEW BATCH");
+                        Console.WriteLine("7.UPDATE MARKS OF STUDENT");
+                        Console.WriteLine("8.EXITING APPLICATION");
+                        Console.WriteLine("======================================================================");
+                        Console.WriteLine("ENTER YOUR CHOICE:");
+                        int choice = Convert.ToInt32(Console.ReadLine());
+                        int rollno;
+                        string batchid;
+                        string name;
+                        string contact;
+                        string address;
+                        int mark;
+                        string trainerid;
 
-                        Class1.insertRecords(rollno, name,contact, address, email, trainerid, batch, marks1, marks2);
-                        break;
-                case 2: break;
-                case 3: break;
-                case 4:
-                    Console.WriteLine("Displaying the Records");
-                    Class1.Display();
-                    break;
-                case 5:
-                    Environment.Exit(0);
-                    break;
-                default:
-                    Console.WriteLine("Invalid Choice");
-                    break;
-            }
+                        
 
-            Console.WriteLine("Do you wish to continue");
-            reply = Convert.ToChar(Console.ReadLine());
-        }while(reply=='y');
-            if(reply!='y'|| reply != 'Y')
-            {
-                Console.WriteLine("Records Inserted");
+                        switch (choice)
+                        {
+                            case 1:
+                                Console.WriteLine("Enter Your details Rollno, Name, contact, address, email, trainerid, batchID, marks1, and marks2");
+                                rollno = Convert.ToInt32(Console.ReadLine());
+                                name = Console.ReadLine();
+                                contact = Console.ReadLine();
+                                address = Console.ReadLine();
+                                
+                                trainerid = Console.ReadLine();
+                                batchid = Console.ReadLine();
+                                
+
+                                // display trainer and batch details 
+
+                                //Class1.DisplayTrainer();
+                                //Class1.DisplayBatch();
+
+                                Class1.insertRecords(rollno, name, contact, address, trainerid, batchid);
+                                break;
+                            case 2:
+                                Console.WriteLine("Enter RollNo Of student");
+                                rollno = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine("Enter New Contact");
+                                contact = Console.ReadLine();
+                                Class1.updateContact(contact, rollno);
+                                break;
+                            case 3:
+                                Console.WriteLine("Enter RollNo");
+                                rollno = Convert.ToInt32(Console.ReadLine());
+                                Class1.deleterecord(rollno);
+                                break;
+                            case 4:
+                                Console.WriteLine("=======================================Displaying the Records===============================");
+                                Class1.Display();
+                                break;
+                            case 5:
+                                Console.WriteLine("Enter TrainerId");
+                                trainerid = Console.ReadLine();
+                                Class1.DisplayTrainer(trainerid);
+                                break;
+                            case 6:
+                                Console.WriteLine("Enter BatchId");
+                                batchid = Console.ReadLine();
+                                Class1.DisplayBatch(batchid);
+                                break;
+                            case 7:
+                                Console.WriteLine("Enter RollNo");
+                                rollno = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine("Enter Marks");
+                                mark = Convert.ToInt32(Console.ReadLine());
+                                Class1.updatemarks(mark,rollno);
+                                break;
+                            case 8:
+                                Environment.Exit(0);
+                                break;
+                            default:
+                                Console.WriteLine("Invalid Choice");
+                                break;
+                        }
+
+
+
+                        Console.WriteLine("Do you wish to continue");
+                        reply = Convert.ToChar(Console.ReadLine());
+                    } while (reply == 'y');
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("========================= Reports=======================");
+                    Console.WriteLine("1. Top 5 Merit List for Specific Batch");
+                    Console.WriteLine("2.Top 10 Merit List For All Batches");
+                    Console.WriteLine("3.Top BatchName & TrainerName");
+                    Console.WriteLine("4.Low Performance Batch");
+                    Console.WriteLine("5.EXIT");
+                    Console.WriteLine("0.Return to Main Menu");
+                    Console.WriteLine("ENTER CHOICE:");
+                    int c = Convert.ToInt32(Console.ReadLine());
+                    string batchid1;
+                    switch (c)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter BatchId");
+                            batchid1 = Console.ReadLine();
+                            Class1.topfive(batchid1);
+                            break;
+                        case 2:
+                            Class1.topten();
+                            break;
+                         case 3:
+                            Class1.Top();
+                            break;
+                        case 4:
+                            Class1.lowscore();
+                            break;
+                        case 5:
+                            Environment.Exit(0);
+                            break;
+                        case 0:
+                            return;
+                           
+                        default:
+                            Console.WriteLine("Invalid Choice");
+                            break;
+                    }
+
+                    break;
             }
             Console.ReadLine();
+
+
         }
     }
 }
